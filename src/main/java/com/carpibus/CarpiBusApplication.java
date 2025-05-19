c4fd6cb6346d04743cf31552dda7dcb51104c10c
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-05-18 21:51:40 -0300

    Initial Commit

---
+++ src/main/java/com/carpibus/CarpiBusApplication.java
@@ -0,0 +1,13 @@
+package com.carpibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpiBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpiBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carpibus/CarpiBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpiBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
